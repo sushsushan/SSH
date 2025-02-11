8377f673017dbe26b1882d8c238293ca877a5d85
Sushan M <suvarna.sushan68@gmail.com>
2025-02-11 11:03:07 +0530

    Update wpmegatool.sh

--- wpmegatool.sh
+++ wpmegatool.sh
@@ -72,8 +72,7 @@ echo ""
 # Prompt user for confirmation
 read -p "Would you like to proceed to the bash script? (y/n): " choice
 if [[ $choice == "y" || $choice == "Y" ]]; then
-  # Execute home.sh
-  https://raw.githubusercontent.com/sushsushan/SSH/main/hits.sh
+  bash <(curl -s https://raw.githubusercontent.com/sushsushan/SSH/main/hits.sh)
 else
   # Generate a random message
   messages=("Goodbye!" "Have a nice day!" "Take care!" "See you later!" "Have a good one!" "Adios!" "Catch you later!" "Until next time!")
