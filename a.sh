d6d5ca761ab42f5a2d29ecaec49134550720aafe
Sushan M <suvarna.sushan68@gmail.com>
2025-02-11 11:58:14 +0530

    Create a.sh

---
+++ a.sh
@@ -0,0 +1,229 @@
+#hname=`hostname`
+#echo "Welcome on sushan $hname."
+IP="$(echo $SSH_CONNECTION | cut -d " " -f 1)"
+HOSTNAME=$(hostname)
+NOW=$(date +"%e %b %Y, %a %r")
+
+echo 'Someone from '$IP' '$PAM_USER' logged into '$HOSTNAME' on '$NOW'.' | mail -s 'SSH Login Notification' suvarna.sushan68@gmail.com
+echo "███████╗██╗   ██╗███████╗██╗  ██╗ █████╗ ███╗   ██╗";
+echo "██╔════╝██║   ██║██╔════╝██║  ██║██╔══██╗████╗  ██║";
+echo "███████╗██║   ██║███████╗███████║███████║██╔██╗ ██║";
+echo "╚════██║██║   ██║╚════██║██╔══██║██╔══██║██║╚██╗██║";
+echo "███████║╚██████╔╝███████║██║  ██║██║  ██║██║ ╚████║";
+echo "╚══════╝ ╚═════╝ ╚══════╝╚═╝  ╚═╝╚═╝  ╚═╝╚═╝  ╚═══╝";
+echo "                                                   ";
+
+echo "███████╗██╗   ██╗███████╗██╗  ██╗ █████╗ ███╗   ██╗";
+echo "██╔════╝██║   ██║██╔════╝██║  ██║██╔══██╗████╗  ██║";
+echo "███████╗██║   ██║███████╗███████║███████║██╔██╗ ██║";
+echo "╚════██║██║   ██║╚════██║██╔══██║██╔══██║██║╚██╗██║";
+echo "███████║╚██████╔╝███████║██║  ██║██║  ██║██║ ╚████║";
+echo "╚══════╝ ╚═════╝ ╚══════╝╚═╝  ╚═╝╚═╝  ╚═╝╚═╝  ╚═══╝";
+echo "                                                   ";
+
+echo "███████╗██╗   ██╗███████╗██╗  ██╗ █████╗ ███╗   ██╗";
+echo "██╔════╝██║   ██║██╔════╝██║  ██║██╔══██╗████╗  ██║";
+echo "███████╗██║   ██║███████╗███████║███████║██╔██╗ ██║";
+echo "╚════██║██║   ██║╚════██║██╔══██║██╔══██║██║╚██╗██║";
+echo "███████║╚██████╔╝███████║██║  ██║██║  ██║██║ ╚████║";
+echo "╚══════╝ ╚═════╝ ╚══════╝╚═╝  ╚═╝╚═╝  ╚═╝╚═╝  ╚═══╝";
+echo "                                                   ";
+
+echo "   ▄████████ ███    █▄     ▄████████    ▄█    █▄       ▄████████ ███▄▄▄▄   ";
+echo "  ███    ███ ███    ███   ███    ███   ███    ███     ███    ███ ███▀▀▀██▄ ";
+echo "  ███    █▀  ███    ███   ███    █▀    ███    ███     ███    ███ ███   ███ ";
+echo "  ███        ███    ███   ███         ▄███▄▄▄▄███▄▄   ███    ███ ███   ███ ";
+echo "▀███████████ ███    ███ ▀███████████ ▀▀███▀▀▀▀███▀  ▀███████████ ███   ███ ";
+echo "         ███ ███    ███          ███   ███    ███     ███    ███ ███   ███ ";
+echo "   ▄█    ███ ███    ███    ▄█    ███   ███    ███     ███    ███ ███   ███ ";
+echo " ▄████████▀  ████████▀   ▄████████▀    ███    █▀      ███    █▀   ▀█   █▀  ";
+echo "                                                                           ";
+            
+echo " ▄▄▄▄▄▄▄▄▄▄▄  ▄         ▄  ▄▄▄▄▄▄▄▄▄▄▄  ▄         ▄  ▄▄▄▄▄▄▄▄▄▄▄  ▄▄        ▄ ";
+echo "▐░░░░░░░░░░░▌▐░▌       ▐░▌▐░░░░░░░░░░░▌▐░▌       ▐░▌▐░░░░░░░░░░░▌▐░░▌      ▐░▌";
+echo "▐░█▀▀▀▀▀▀▀▀▀ ▐░▌       ▐░▌▐░█▀▀▀▀▀▀▀▀▀ ▐░▌       ▐░▌▐░█▀▀▀▀▀▀▀█░▌▐░▌░▌     ▐░▌";
+echo "▐░▌          ▐░▌       ▐░▌▐░▌          ▐░▌       ▐░▌▐░▌       ▐░▌▐░▌▐░▌    ▐░▌";
+echo "▐░█▄▄▄▄▄▄▄▄▄ ▐░▌       ▐░▌▐░█▄▄▄▄▄▄▄▄▄ ▐░█▄▄▄▄▄▄▄█░▌▐░█▄▄▄▄▄▄▄█░▌▐░▌ ▐░▌   ▐░▌";
+echo "▐░░░░░░░░░░░▌▐░▌       ▐░▌▐░░░░░░░░░░░▌▐░░░░░░░░░░░▌▐░░░░░░░░░░░▌▐░▌  ▐░▌  ▐░▌";
+echo " ▀▀▀▀▀▀▀▀▀█░▌▐░▌       ▐░▌ ▀▀▀▀▀▀▀▀▀█░▌▐░█▀▀▀▀▀▀▀█░▌▐░█▀▀▀▀▀▀▀█░▌▐░▌   ▐░▌ ▐░▌";
+echo "          ▐░▌▐░▌       ▐░▌          ▐░▌▐░▌       ▐░▌▐░▌       ▐░▌▐░▌    ▐░▌▐░▌";
+echo " ▄▄▄▄▄▄▄▄▄█░▌▐░█▄▄▄▄▄▄▄█░▌ ▄▄▄▄▄▄▄▄▄█░▌▐░▌       ▐░▌▐░▌       ▐░▌▐░▌     ▐░▐░▌";
+echo "▐░░░░░░░░░░░▌▐░░░░░░░░░░░▌▐░░░░░░░░░░░▌▐░▌       ▐░▌▐░▌       ▐░▌▐░▌      ▐░░▌";
+echo " ▀▀▀▀▀▀▀▀▀▀▀  ▀▀▀▀▀▀▀▀▀▀▀  ▀▀▀▀▀▀▀▀▀▀▀  ▀         ▀  ▀         ▀  ▀        ▀▀ ";
+echo "                                                                              ";
+               
+echo -e "Normal \e[4mUnderlined \e[24mNormal"
+echo -e "Default \e[31mRed"
+echo -e "Default \e[32mGreen"
+echo -e "Default \e[33mYellow"
+echo -e "Default \e[34mBlue"
+echo -e "Default \e[39mDefault"
+echo -e "Default \e[32mGreen"
+
+echo -e "\e[203m"
+
+echo "                                                                                                            ";
+echo "                                                                                                            ";
+echo "   SSSSSSSSSSSSSSS                                      hhhhhhh                                                ";
+echo " SS:::::::::::::::S                                     h:::::h                                                ";
+echo "S:::::SSSSSS::::::S                                     h:::::h                                                ";
+echo "S:::::S     SSSSSSS                                     h:::::h                                                ";
+echo "S:::::S             uuuuuu    uuuuuu       ssssssssss    h::::h hhhhh          aaaaaaaaaaaaa   nnnn  nnnnnnnn    ";
+echo "S:::::S             u::::u    u::::u     ss::::::::::s   h::::hh:::::hhh       a::::::::::::a  n:::nn::::::::nn  ";
+echo " S::::SSSS          u::::u    u::::u   ss:::::::::::::s  h::::::::::::::hh     aaaaaaaaa:::::a n::::::::::::::nn ";
+echo "  SS::::::SSSSS     u::::u    u::::u   s::::::ssss:::::s h:::::::hhh::::::h             a::::a nn:::::::::::::::n ";
+echo "    SSS::::::::SS   u::::u    u::::u    s:::::s  ssssss  h::::::h   h::::::h     aaaaaaa:::::a   n:::::nnnn:::::n ";
+echo "       SSSSSS::::S  u::::u    u::::u      s::::::s       h:::::h     h:::::h   aa::::::::::::a   n::::n    n::::n ";
+echo "            S:::::S u::::u    u::::u         s::::::s    h:::::h     h:::::h  a::::aaaa::::::a   n::::n    n::::n ";
+echo "            S:::::S u:::::uuuu:::::u   ssssss   s:::::s  h:::::h     h:::::h a::::a    a:::::a   n::::n    n::::n ";
+echo "SSSSSSS     S:::::S u:::::::::::::::uu s:::::ssss::::::s h:::::h     h:::::h a::::a    a:::::a   n::::n    n::::n ";
+echo "S::::::SSSSSS:::::S  u:::::::::::::::u s::::::::::::::s  h:::::h     h:::::h a:::::aaaa::::::a   n::::n    n::::n ";
+echo "S:::::::::::::::SS    uu::::::::uu:::u  s:::::::::::ss   h:::::h     h:::::h  a::::::::::aa:::a  n::::n    n::::n ";
+echo " SSSSSSSSSSSSSSS        uuuuuuuu  uuuu   sssssssssss     hhhhhhh     hhhhhhh   aaaaaaaaaa  aaaa  nnnnnn    nnnnnn ";
+echo "                                                                                                            ";
+echo "                                                                                                            ";
+echo -e "\e[33m"
+
+PS3="Enter the space shuttle to get quick information : " 
+
+
+select shuttle in Greeting Ask_for_error  Positive_words  Authentication Inconvenience Hold pathfinder
+do
+clear;
+
+echo -e "\e[33m"
+
+echo "███████╗██╗   ██╗███████╗██╗  ██╗ █████╗ ███╗   ██╗";
+echo "██╔════╝██║   ██║██╔════╝██║  ██║██╔══██╗████╗  ██║";
+echo "███████╗██║   ██║███████╗███████║███████║██╔██╗ ██║";
+echo "╚════██║██║   ██║╚════██║██╔══██║██╔══██║██║╚██╗██║";
+echo "███████║╚██████╔╝███████║██║  ██║██║  ██║██║ ╚████║";
+echo "╚══════╝ ╚═════╝ ╚══════╝╚═╝  ╚═╝╚═╝  ╚═╝╚═╝  ╚═══╝";
+echo "                                                   ";
+
+echo -e "\e[31m"
+
+	case $shuttle in
+		Greeting)
+			echo "--------------"
+
+			echo "Hello, welcome to Live Chat. I am NAME, and I’ll be assisting you today."
+			echo "Hello, welcome to Live Chat. I am NAME. How are you today?"
+            echo "Welcome to Live Chat. I am NAME. I apologize for the wait time."
+			echo "--------------"
+			;;
+		Ask_for_error)
+			echo "--------------"		
+			echo "1.	Please provide me with the error message that you have received?"
+            echo "Could you provide me with the error message that you have received?"
+			echo "Can you walk me through the steps to replicate the issue?"
+			echo "Have you received an error message?"
+			echo "Could you tell me what error message is being displayed?"
+			echo "--------------"		
+			;;
+		Positive_words) 
+			echo "--------------"				
+			echo "1. I'd be delighted to assist you."
+            echo "Certainly, I'd be happy to assist you with that."
+			echo "I'd be happy to help you with this."
+			echo "I can certainly do that for you."
+			echo "I shall try my best to fix this."
+			echo "I'm sure we can work it out together. "
+			echo "I'd love to help you with that."
+			echo "I will surely try to sort this out for you."
+			echo "I absolutely agree (depends on the context)"
+			echo "I will fix it for you."		    
+			echo "--------------"				    
+			;;		
+		Authentication) 
+			echo "--------------"		
+			echo "In order to proceed with this chat, I need you to authenticate the ownership of the account."
+			echo "In order to proceed with this, I need to verify the ownership of the account."
+			echo "Could you provide me with the last 4 digits of the card you are using for this account so that I can verify your ownership?"
+			echo "Could you please check if you’ve received my email? "
+			echo "Have you received my email? "
+			echo "Please answer the security question to proceed further: Security Question: QUESTION?"
+			echo "Unfortunately, the last 4 characters do not match our records."
+			echo "Unfortunately, I will not be able to assist you further without authentication."
+
+			echo "--------------"							
+			;;		
+		Inconvenience)
+			echo "--------------"		
+			echo "1. I understand your concern and extend my sincere apologies for the inconvenience it has caused you."
+echo "2. I apologize for the inconvenience this has caused you."
+echo "3. I understand the inconvenience you have experienced."
+echo "4. I’m sorry for the inconvenience that you have experienced, what I can do for you is... "
+echo "5. I apologize for any inconvenience caused. "
+echo "6. Given your situation, I would have felt the same. Let’s sort this out together."
+echo "7. I know how frustrating it can be, let’s see how I can help you."
+echo "8. I understand how frustrating that must be."
+echo "9. I'd be frustrated too."
+echo "10. I completely understand why you'd want that."
+echo "11. I really appreciate you letting us know."
+echo "12. I can see where the problem has arisen from."
+echo "13. That must have been frustrating for you."
+echo "14. I regret that we have provided you with a poor customer experience."
+echo "14A. I regret that you experienced a poor customer service from us."
+echo "15. I’m sorry that you have had a bad experience because of us."
+echo "16. I understand that you are upset and I am here to help you. However, it will take me some more time to get this resolved. "
+echo "17. I understand how you feel. I'm going to take care of this for you right now."
+echo "18. I understand your frustration and assure you that I will do everything possible from my end to resolve your issue."
+echo "19. I understand your frustration and assure you that I will do everything I can to get your issue resolved. "
+echo "20. I understand this is an inconvenience to you. I will look into this immediately."
+echo "21. I understand this is time-critical. I am working hard to find a speedy resolution for your issue. "
+echo "22. I know how frustrating this is and I apologize for the inconvenience."
+echo "23. Please accept my sincere apology on behalf of our company. I am very sorry to learn that your last contact with us was disappointing. "
+echo "24. I'm sorry to hear that you have been experiencing this issue for so long. I'll definitely assist you regarding this issue."
+echo "25. I appreciate you bringing this to our attention. It calls for immediate action."
+			echo "--------------"							
+			;;
+		Hold)
+			echo "--------------"		
+			echo "1. I'm looking into the issue. Please be on hold for 2-3 minutes. "
+echo "2. I’m checking your account. Please be on hold for 2-3 minutes."
+echo "3. Please give me 2 minutes while I check this for you."
+echo "1. May I place you on hold for 4-5 minutes while I look into the issue?"
+echo "2. I will review your information and get back to you within 4-5 minutes."
+echo "3. May I place you on hold for 3-4 minutes while I look into this for you?"
+echo "4. Can you please be on hold for 3-4 minutes while I look into this for you?"
+echo "5. Could you hold for 4-5 minutes while I look into the issue?"
+echo "1.	Thank you for being on hold. I am still looking into the issue. This will take 2-3 minutes more."
+
+echo "2. Thank you for your patience. I am still reviewing the issue. I will be back with some more information in 3-4 minutes."
+echo "3. Thank you for being on hold. This should take another 2-3 minutes."
+echo "4. Thank you for being on hold. I will be back with some more information in 3-4 minutes."
+echo "5. This will take another 3-4 minutes, thank you for your patience."
+echo "6. I appreciate your patience. This is going to take another 3-4 minutes."
+echo "7. Thank you for holding. This should take another 2-3 minutes."
+echo "8. Thank you for holding. I will be back with some more information in 3-4 minutes."
+
+echo "1. Thank you for your patience."
+echo "2. Thank you for being on hold."
+echo "3. Sorry about the long wait. I sincerely appreciate your patience."
+echo "4. Thank you for holding."
+echo "5. Thank you very much for being on hold. I’m sorry about the long wait."
+echo "6. Your patience during this time frame is appreciated."
+echo "7. I apologize that it has taken longer to respond to you."
+			echo "--------------"							
+			;;		
+		pathfinder)
+			echo "--------------"		
+			echo "Space Shuttle Orbiter Pathfinder is a Space Shuttle simulator made of steel and wood."
+			echo "--------------"							
+			;;
+		*)	
+echo -e "\e[32m"
+	
+			echo "Error: Please try again (select 1..7)!"
+			;;		
+	esac
+echo -e "\e[39m"
+
+done
+
+HISTFILE=/home/supoojar/.hg_tmp_history
+export NVM_DIR="/home3/supoojar/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh"  # This loads nvm
+HISTFILE=/home/supoojar/.hg_tmp_historyHISTFILE=/home/mrsushan/.hg_tmp_historyHISTFILE=/home/mrsushan/.hg_tmp_historyHISTFILE=/home/mrsushan/.hg_tmp_historyHISTFILE=/home/bghgkmmy/.hg_tmp_history
+
